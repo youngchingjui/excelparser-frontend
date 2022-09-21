4f53a381435c9e5afff95d41314092116e7f915d
youngchingjui <young.chingjui@gmail.com>
2022-09-21 14:24:38 +0800

    updated layout

--- pages/scripts/[script].js
+++ pages/scripts/[script].js
@@ -1,33 +1,45 @@
-import { useRouter } from "next/router"
 import Button from "react-bootstrap/Button"
+import Col from "react-bootstrap/Col"
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Row from "react-bootstrap/Row"
+import { useRouter } from "next/router"
 
 const Script = () => {
   const router = useRouter()
   const { script } = router.query
 
   return (
-    <div>
-      <div>Header</div>
-      <div>Sidebar</div>
-      <div>
-        Excel window
-        <div>Decode from GB-18030 to UTF-8</div>
-        <div>Remove first 4 lines</div>
-        <div>Replace headers:</div>
-        <div>&quot;交易时间&quot; --&gt; &quot;date&quot;</div>
-        <div>&quot;交易对方&quot; --&gt; &quot;payee&quot;</div>
-        <div>&quot;商品&quot; --&gt; &quot;notes&quot;</div>
-        <div>&quot;金额（元）&quot; --&gt; &quot;amount&quot;</div>
-        <div>Converts date column to actual date format</div>
-        <div></div>
-        <div>
-          If 收/支 column is &quot;支出&quot;j, then amount is multiplied by -1
-        </div>
-        <div>If notes == &quot;/&quot;, copy 交易类型 into 商品</div>
-        <div>Returns the following columns: date, payee, notes, amount</div>
-        <Button>Add next instruction</Button>
-      </div>
-    </div>
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>Excel Parser</Navbar.Brand>
+        </Container>
+      </Navbar>
+      <Container>
+        <Row>
+          <Col>
+            <div>Decode from GB-18030 to UTF-8</div>
+            <div>Remove first 4 lines</div>
+            <div>Replace headers:</div>
+            <div>&quot;交易时间&quot; --&gt; &quot;date&quot;</div>
+            <div>&quot;交易对方&quot; --&gt; &quot;payee&quot;</div>
+            <div>&quot;商品&quot; --&gt; &quot;notes&quot;</div>
+            <div>&quot;金额（元）&quot; --&gt; &quot;amount&quot;</div>
+            <div>Converts date column to actual date format</div>
+            <div></div>
+            <div>
+              If 收/支 column is &quot;支出&quot;, then amount is multiplied by
+              -1
+            </div>
+            <div>If notes == &quot;/&quot;, copy 交易类型 into 商品</div>
+            <div>Returns the following columns: date, payee, notes, amount</div>
+            <Button>Add next instruction</Button>
+          </Col>
+          <Col xs="8">Excel window</Col>
+        </Row>
+      </Container>
+    </>
   )
 }
 
