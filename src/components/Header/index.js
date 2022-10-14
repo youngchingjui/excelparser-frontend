ea09f9b688bb860124b0e42dc4c2133d683172fb
youngchingjui <young.chingjui@gmail.com>
2022-10-14 12:42:06 +0800

    added `scripts` link to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,20 @@
 import Link from "next/link"
 import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
 import Navbar from "react-bootstrap/Navbar"
 
 const Header = () => {
   return (
     <Navbar bg="dark" variant="dark">
       <Container>
-        <Link href="/">
-          <Navbar.Brand>Excel Parser</Navbar.Brand>
-        </Link>
+        <Nav>
+          <Link href="/" passHref>
+            <Navbar.Brand>Excel Parser</Navbar.Brand>
+          </Link>
+          <Link href="/scripts" passHref>
+            <Nav.Link>Scripts</Nav.Link>
+          </Link>
+        </Nav>
       </Container>
     </Navbar>
   )
